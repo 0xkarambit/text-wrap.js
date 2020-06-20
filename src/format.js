91398cd577e8fe1f9e5615f5d43f537567da49be
HarshitJoshi9152 <harsjoshi1614@gmail.com>
2020-06-20 17:15:16 +0530

    corrected newfmt, added -f flag, fixed options errors etc

--- src/format.js
+++ src/format.js
@@ -1,48 +1,80 @@
-function prettyPaste(string, limit = 80, delimiter) {
+function prettyPaste(string, limit = 80, delimiter = "\n") {
 	// limit : character limit per line
+	// replaces the whitespace nearest to the limit (before limit) Index with a newline.
 	let template;
 	if (string.length > limit) {
-		let breakIndex = string.slice(0, limit).lastIndexOf(" ");
-		template = string.slice(0, breakIndex) + delimiter + prettyPaste(string.slice(breakIndex + 1), limit, delimiter);
+		let breakIndex = string.substring(0, limit).lastIndexOf(" ");
+		template =
+			string.substring(0, breakIndex) +
+			delimiter +
+			prettyPaste(string.substring(breakIndex + 1), limit, delimiter);
 	}
-	return template = template || string;
+	return (template = template || string);
+}
+// it keeps on re-processing the same line if given an input without whitespace in 80 chars
+
+function prettyPasteV2(string, limit = 80, delimiter = "\n") {
+	let template;
+	if (string.length > limit) {
+		let breakIndex = string.substring(0, limit).lastIndexOf(" ");
+		if (breakIndex == -1) {
+			breakIndex = limit - 1; // 0 - 79 = 80 chars (limit)
+			template =
+				string.substring(0, breakIndex) +
+				delimiter +
+				prettyPasteV2(string.substring(breakIndex), limit, delimiter);
+		} else {
+			template =
+				string.substring(0, breakIndex) +
+				delimiter +
+				prettyPasteV2(string.substring(breakIndex + 1), limit, delimiter);
+		}
+	}
+	return (template = template || string);
 }
 
-
-function newfmt(str, limit = 80, del = '\n') {
-	console.time('newfmt');
-	let temp = '';
+function newfmt(str, limit = 80, del = "\n") {
+	let temp = "";
 	let start = 0;
 	const len = str.length;
-	for (let i = 0; i < len; i+=limit) {
+	for (let i = 0; i < len; i += limit) {
 		// code
 		temp += str.slice(start, i) + del;
 		start = i;
 	}
-	console.timeEnd('newfmt')
-	return temp;
+	return temp + str.slice(start);
 }
 
-// tests
-
+// TESTS
 // this if statement implies that this file has been called on its own and my imported by anyother file
 if (require.main == module) {
-	let str = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci voluptates quia accusantium corporis a, omnis eveniet non neque numquam? Et ad doloremque id minus corrupti unde sapiente, repudiandae molestias minima ab aspernatur qui ex officia voluptas accusamus amet pariatur animi laudantium alias mollitia, in maxime quam! Culpa deserunt necessitatibus quod?;'
-	console.log(newfmt(str, 20, '\r\n'))
-	console.time('prettyPaste');
-	console.log(prettyPaste(str, 20, '\r\n'))
-	console.timeEnd('prettyPaste');
-}
+	const { log, time, timeEnd } = console;
+	let str =
+		process.argv[2] ||
+		"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci voluptates quia accusantium corporis a, omnis eveniet non neque numquam? Et ad doloremque id minus corrupti unde sapiente, repudiandae molestias minima ab aspernatur qui ex officia voluptas accusamus amet pariatur animi laudantium alias mollitia, in maxime quam! Culpa deserunt necessitatibus quod?;" ||
+		"hello wordl hello wordl hello wordl hello wordl hello hello wordl hello wordl hello wordl hello wordl hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhe he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he he";
+	time("newfmt");
+	log(newfmt(str, 20, "\r\n"));
+	newfmt(str, 20, "\r\n");
+	timeEnd("newfmt");
 
-// performace test 
+	time("prettyPaste");
+	log(prettyPaste(str, 20, "\r\n"));
+	prettyPaste(str, 20, "\r\n");
+	timeEnd("prettyPaste");
+}
+// performace test [old]
 // newfmt: 0.162ms
 // prettyPaste: 0.753ms
 
-
-// issues 
-//error with 'hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh']
+// performace test [new]
+// newfmt: 0.225ms
+// prettyPaste: 0.141ms
 
 module.exports = {
 	prettyPaste,
-	newfmt
-}
+	newfmt,
+	prettyPasteV2,
+};
+
+// todo well the given input can also have newlines huh maybe we should reset the checking of whitespaces when we encounter a newline.
--- src/index.js
+++ src/index.js
@@ -1,41 +1,65 @@
 #!/usr/bin/env node
-const fmt = require('./format')
-const readline = require('readline');
+const fmt = require("./format");
+const readline = require("readline");
 const reader = readline.createInterface({
-	input:process.stdin,
-	output:process.stdout,
-})
-const { program } = require('commander');
+	input: process.stdin,
+	output: process.stdout,
+});
+const { program } = require("commander");
+const { write: copy } = require("clipboardy");
 
 const log = console.log;
+const red = "\x1b[96m"; // 2 green. 4 blue. 3 yellow 5.purple
+const clear = "\x1b[0m";
 
 program
 	.version("v0.0.1")
 	.option("-l, --limit <num>", "set max line length to <num>")
-	.option("-d, --delimiter", "set the delimiter to use [default] \n")
+	.option("-d, --delimiter <char>", "set the delimiter to use [default] \\n") // this is currently char at end of line
 	.option("-t, --text <text>", "set the line of text to modify")
-	.option('-n, --no-copy', 'specifies to not copy the formated text to clipboard by default')
+	.option("-f, --force", "uses force format method to wrap text")
+	.option("-c, --copy", "specifies to copy the formated text to clipboard")
 	.parse(process.argv);
 
 (async function main() {
-	// main program flow
-	const limit = program.limit ? program.limit : 80;
-	const delimiter = (program.delimiter) ? (program.delimiter) : '\n';
+	const limit = program.limit ? +program.limit : 80;
+	let delimiter = processDelimiter(
+		program.delimiter ? program.delimiter : "\n"
+	);
 	const text = program.text ? program.text : await getText("enter text> ");
-	const result = fmt.prettyPaste(text, limit, delimiter);
-	log(result)
-})()
+	const method = program.force ? "newfmt" : "prettyPasteV2";
 
+	// const result = fmt.prettyPasteV2(text, limit, delimiter);
+	const result = fmt[method](text, limit, delimiter);
+
+	// process.stdin.write("\n" + result); // gives error -> write EPIPE
+	writeResult(result);
+	program.copy ? await copy(result) : null;
+	process.exit(0);
+})();
 
 // input stops when it encounters a new line should this be alowed to happen ?
 
-
-
 function getText(prompt) {
-	return new Promise( resolve => {
-		reader.question(prompt, (answer)=>{
-			reader.close();
+	return new Promise((resolve) => {
+		reader.question(prompt, (answer) => {
+			reader.close(); // what if we want it to run in an interactive mode.
 			resolve(answer);
 		});
 	});
 }
+
+function writeResult(str) {
+	log("\n" + red + str + clear);
+}
+
+function processDelimiter(str) {
+	// str is a char
+	let string = "";
+	return str.replace("\\n", "\n");
+}
+
+// todo add ability to specify to // break at whitespaces or force break
+// ! the program is not exiting on its own ! why ?
+// should i add config support to save the preferences ?
+// add ifs flag
